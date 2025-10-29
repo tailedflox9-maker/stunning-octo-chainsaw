4d2e4bc1b4ebd67643f2e9baed4f04b8957f6493
tailedflox9-maker <tailedflox9@gmail.com>
2025-10-29 20:15:03 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Aptos-Mono', 'Inter', 'sans-serif'],
+        mono: ['Aptos-Mono', 'monospace'],
+      },
+    },
   },
   plugins: [
     require('tailwindcss-animate'), // Add this plugin
